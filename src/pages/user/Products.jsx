67de364d0c83e7d95d5fc73dfef1b8300f020064
Juan2040751 <viafara.juan@correounivalle.edu.co>
2024-04-16 11:04:48 -0500

    CLW-100: imagenes productos

--- src/pages/user/Products.jsx
+++ src/pages/user/Products.jsx
@@ -1,42 +1,39 @@
 import React, { useState, useEffect } from "react";
-import Commerce from "@chec/commerce.js";
-import NavigationBar from "../../components/NavigationBar";
-import WhatsApp from "../../components/WhatsApp"; 
+import { useParams } from 'react-router-dom';
+import WhatsApp from "../../components/WhatsApp";
 import InfoBar from "./InfoBar";
 import "./products.css";
+import axios from 'axios';
 
 function Products() {
-  const commerce = new Commerce(
-    "pk_5672597a83b6b6dc1f2710eab13845691c6bbfade188b"
-  );
-
   const [productos, setProductos] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(null);
   const [loading, setLoading] = useState(true);
+  const { categoria } = useParams();
 
   useEffect(() => {
-    async function fetchProducts() {
+    const fetchData = async () => {
+      setLoading(true);
       try {
-        setLoading(true);
-        const { data: products } = await commerce.products.list({
-          category_slug: selectedCategory,
-        });
-        setProductos(products);
-        setLoading(false);
-        console.log("Productos:", products);
+        let response;
+        if (categoria) {
+          response = await axios.get("http://127.0.0.1:8000/products/detalles-por-categoria/${categoria}/");
+        } else {
+          response = await axios.get('http://localhost:8000/products/view-details-products/');
+        }
+        setProductos(response.data);
       } catch (error) {
-        console.error("Error fetching products:", error);
-        setLoading(false);
+        console.error('Error al obtener los datos de productos:', error);
       }
-    }
+      setLoading(false);
+    };
 
-    fetchProducts();
-  }, [selectedCategory]);
+    fetchData();
+  }, [categoria]);
 
   return (
     <div className="productsPage">
       {/* <NavigationBar onSelectCategory={setSelectedCategory} /> */}
-      <h5 style={{ marginTop: "10px", marginBottom: "10px", marginLeft: "10px"}}>Productos</h5>
+      <h5 style={{ marginTop: "10px", marginBottom: "10px", marginLeft: "10px" }}>Productos</h5>
       <div className="productsContainer">
         {loading ? (
           <p>Cargando productos...</p>
@@ -46,9 +43,9 @@ function Products() {
           productos.map((producto) => (
             <div key={producto.id} className="productItem">
               <div className="productContent">
-                <img src={producto.image.url} alt={producto.name} />
-                <h6>{producto.name}</h6>
-                <p>${producto.price.formatted}</p>
+                <img src={producto.imagenes[0]} />
+                <h6>{producto.nombre}</h6>
+                <p>${producto.precio}</p>
               </div>
               <div className="productButtons">
                 <button className="buttonAddCar">AÑADIR AL CARRITO</button>
@@ -65,4 +62,3 @@ function Products() {
 }
 
 export default Products;
-
