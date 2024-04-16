a87760194eda0cc610f55d4945362e12575fa84f
Velyxius <dadawdasd@hotmail.com>
2024-04-16 03:38:58 -0500

    CLW-15: Changed queries to use Axios for requests instead of fetch()

--- src/api/Products.jsx
+++ src/api/Products.jsx
@@ -1,26 +1,36 @@
+import axios from "axios";
+
+const url = "http://127.0.0.1:8000/products/";
+
 export async function consultarProductos(config) {
-  const data = await fetch(
-    `http://127.0.0.1:8000/products/view-products/`,
-    config
-  );
-  console.log(data);
-  return data.json();
+  try {
+    const response = await axios.get(url + `view-products/`, config);
+    // console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching products: ", error);
+  }
 }
 
 export async function consultarDetalles(config) {
-  const data = await fetch(
-    `http://127.0.0.1:8000/products/view-details/`,
-    config
-  );
-  console.log(data);
-  return data.json();
+  try {
+    const response = await axios.get(url + `view-details/`, config);
+    // console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching product details: ", error);
+  }
 }
 
-export async function consultarImagenes(config) {
-  const data = await fetch(
-    `http://127.0.0.1:8000/products/view-products/`,
-    config
-  );
-  console.log(data);
-  return data.json();
+export async function consultarTrozosDetalles(config, page) {
+  try {
+    const response = await axios.get(
+      url + `view-sliced-details/?page=${page}`,
+      config
+    );
+    // console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching product details: ", error);
+  }
 }
