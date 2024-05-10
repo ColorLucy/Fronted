65f8a75a5a90453cb4a52f13cb574b91085e23ae
Deisy05 <55037333+Deisy05@users.noreply.github.com>
2024-05-10 15:00:40 -0500

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,7 +3,6 @@ import dayjs from "dayjs";
 import { jwtDecode } from "jwt-decode";
 
 const baseURL = "https://colorlucyserver.onrender.com"
-//const baseURL = "http://127.0.0.1:8000"
 
 let authTokens = localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null;
 const axiosInstance = axios.create({
