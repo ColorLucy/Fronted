1c360e626012dd00905b2f042cdc01636554f87b
Velyxius <dadawdasd@hotmail.com>
2024-05-10 05:58:07 -0500

    Manual Merge of all Home Functionalities

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -9,16 +9,40 @@ import {
   Paper,
   Typography,
   useMediaQuery,
+  styled,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import {
+  getHomeInfo,
+  getStartImages,
+  getCombinationsImages,
+  getProductsImages,
+  getAlliesImages,
+} from "../../utils/information";
 
 export default function Home() {
   const isMobileOrTablet = useMediaQuery("(max-width: 960px)");
+
+  const [textInfo, setTextInfo] = useState({});
+  const [startImages, setStartImages] = useState([]);
+  const [loadingStart, setLoadingStart] = useState(true);
+  const [combinationsImages, setCombinationsImages] = useState([]);
+  const [loadingCombinations, setLoadingCombinations] = useState(true);
+  const [productsImages, setProductsImages] = useState([]);
+  const [loadingProducts, setLoadingProducts] = useState(true);
+  const [alliesImages, setAlliesImages] = useState([]);
+  const [loadingAllies, setLoadingAllies] = useState(true);
+
   const [scrollY, setScrollY] = useState(0);
   const [isVisible, setIsVisible] = useState({
     combinaciones: false,
     productos: false,
     aliados: false,
+  });
+
+  const StyledCard = styled(Card)({
+    border: "none",
+    boxShadow: "none",
   });
 
   useEffect(() => {
@@ -75,6 +99,43 @@ export default function Home() {
     };
   }, []);
 
+  useEffect(() => {
+    const fetchTextInfo = async () => {
+      const data = await getHomeInfo();
+      setTextInfo(data[0]);
+      console.log("Home Text: ", data[0]);
+    };
+    const fetchStartImages = async () => {
+      const data = await getStartImages();
+      setStartImages(data);
+      console.log("Start: ", data);
+      setLoadingStart(false);
+    };
+    const fetchCombinationsImages = async () => {
+      const data = await getCombinationsImages();
+      setCombinationsImages(data);
+      console.log("Combinations: ", data);
+      setLoadingCombinations(false);
+    };
+    const fetchProductsImages = async () => {
+      const data = await getProductsImages();
+      setProductsImages(data);
+      console.log("Products: ", data);
+      setLoadingProducts(false);
+    };
+    const fetchAlliesImages = async () => {
+      const data = await getAlliesImages();
+      setAlliesImages(data);
+      setLoadingAllies(false);
+      // console.log("Allies: ", data);
+    };
+    fetchTextInfo();
+    fetchStartImages();
+    fetchCombinationsImages();
+    fetchProductsImages();
+    fetchAlliesImages();
+  }, []);
+
   const animatedPropsCombinaciones = useSpring({
     opacity: isVisible.combinaciones ? 1 : 0,
     transform: `translateY(${isVisible.combinaciones ? 0 : +100}px)`,
@@ -109,78 +170,85 @@ export default function Home() {
         }
       >
         {/* Informacion empresa, a inicio de página, lado izquierdo */}
-        <Grid
-          item
-          xs={12}
-          style={{
-            backgroundImage: `url('homeColorLucy1.png')`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            width: "100%",
-            minHeight: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "white",
-            textAlign: "center",
-            zIndex: 0,
-          }}
-        >
-          {/* Contenido de texto superpuesto */}
-          <Grid container justifyContent="center" alignItems="center">
-            <Grid
-              item
-              xs={10}
-              md={6}
-              style={{
-                position: "relative",
-                zIndex: 1,
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                padding: "20px",
-                borderRadius: "5px",
-              }}
-            >
-              <Typography variant={isMobileOrTablet ? "h4" : "h2"} gutterBottom>
-                Empresa de pintura automotriz
-              </Typography>
-              <Typography
-                variant={isMobileOrTablet ? "body1" : "h6"}
-                gutterBottom
-              >
-                Nos especializamos en combinaciones de pinturas.
-              </Typography>
-              <Typography
-                variant={isMobileOrTablet ? "body2" : "body1"}
-                gutterBottom
-              >
-                ¡Realizamos domicilios en Cali y sus alrededores!
-              </Typography>
-              <Button
-                variant="contained"
-                href="https://api.whatsapp.com/send/?phone=%2B573155176725&text=Hola,%20deseo%20asesor%C3%ADa&type=phone_number&app_absent=0"
-                sx={{
-                  backgroundColor: "white",
-                  marginTop: "30px",
-                  color: "black",
-                  "&:hover": { color: "white" },
+        {loadingStart ? (
+          <></>
+        ) : (
+          <Grid
+            item
+            xs={12}
+            style={{
+              backgroundImage: `url(${startImages[0].url})`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              width: "100%",
+              minHeight: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "white",
+              textAlign: "center",
+              zIndex: 0,
+            }}
+          >
+            {/* Contenido de texto superpuesto */}
+            <Grid container justifyContent="center" alignItems="center">
+              <Grid
+                item
+                xs={10}
+                md={6}
+                style={{
+                  position: "relative",
+                  zIndex: 1,
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  padding: "20px",
+                  borderRadius: "5px",
                 }}
               >
-                Comprar
-              </Button>
+                <Typography
+                  variant={isMobileOrTablet ? "h4" : "h2"}
+                  gutterBottom
+                >
+                  {textInfo.start_title}
+                </Typography>
+                <Typography
+                  variant={isMobileOrTablet ? "body1" : "h6"}
+                  gutterBottom
+                >
+                  {textInfo.start_text_one}
+                </Typography>
+                <Typography
+                  variant={isMobileOrTablet ? "body2" : "body1"}
+                  gutterBottom
+                >
+                  {textInfo.start_text_two}
+                </Typography>
+                <Button
+                  variant="contained"
+                  href="https://api.whatsapp.com/send/?phone=%2B573155176725&text=Hola,%20deseo%20asesor%C3%ADa&type=phone_number&app_absent=0"
+                  sx={{
+                    backgroundColor: "white",
+                    marginTop: "30px",
+                    color: "black",
+                    "&:hover": { color: "white" },
+                  }}
+                >
+                  Comprar
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
+        )}
 
         {/* Apartado de combinaciones */}
         <animated.div style={animatedPropsCombinaciones}>
           <Grid
+            id="combinaciones"
             container
             item
             xs={12}
             justifyContent="center"
             alignItems="center"
-            id="combinaciones"
           >
             <Grid item xs={12} md={12}>
               <Typography
@@ -192,7 +260,7 @@ export default function Home() {
                   textAlign: "center",
                 }}
               >
-                Combinaciones
+                {textInfo.combinations_title}
               </Typography>
             </Grid>
             {/* Imágenes de combinaciones */}
@@ -206,75 +274,35 @@ export default function Home() {
               alignItems="center"
               gap={2}
             >
-              <Grid item xs={12} md={3.5}>
-                <Paper
-                  elevation={0}
-                  sx={{
-                    maxWidth: "100%",
-                    height: "40vh",
-                    width: "100%",
-                    justifyContent: "center",
-                    display: "flex",
-                  }}
-                >
-                  <img
-                    src={"combinaciones1.png"}
-                    alt={"combinaciones1"}
-                    style={{
-                      width: "100%",
-                      maxHeight: "100%",
-                      objectFit: "cover",
-                      borderRadius: "10px",
-                    }}
-                  />
-                </Paper>
-              </Grid>
-              <Grid item xs={12} md={3.5}>
-                <Paper
-                  elevation={0}
-                  sx={{
-                    maxWidth: "100%",
-                    height: "40vh",
-                    width: "100%",
-                    justifyContent: "center",
-                    display: "flex",
-                  }}
-                >
-                  <img
-                    src={"combinaciones2.png"}
-                    alt={"combinaciones2"}
-                    style={{
-                      width: "100%",
-                      maxHeight: "100%",
-                      objectFit: "cover",
-                      borderRadius: "10px",
-                    }}
-                  />
-                </Paper>
-              </Grid>
-              <Grid item xs={12} md={3.5}>
-                <Paper
-                  elevation={0}
-                  sx={{
-                    maxWidth: "100%",
-                    height: "40vh",
-                    width: "100%",
-                    justifyContent: "center",
-                    display: "flex",
-                  }}
-                >
-                  <img
-                    src={"combinaciones3.jpeg"}
-                    alt={"combinaciones3"}
-                    style={{
-                      width: "100%",
-                      maxHeight: "100%",
-                      objectFit: "cover",
-                      borderRadius: "10px",
-                    }}
-                  />
-                </Paper>
-              </Grid>
+              {startImages == null ? (
+                <h3>{":("}</h3>
+              ) : (
+                combinationsImages.map((image, index) => (
+                  <Grid key={index} item xs={12} md={3.5}>
+                    <Paper
+                      elevation={0}
+                      sx={{
+                        maxWidth: "100%",
+                        height: "50vh",
+                        width: "100%",
+                        justifyContent: "center",
+                        display: "flex",
+                      }}
+                    >
+                      <img
+                        src={image.url}
+                        alt={`combinaciones${index + 1}`}
+                        style={{
+                          width: "100%",
+                          maxHeight: "100%",
+                          objectFit: "cover",
+                          borderRadius: "10px",
+                        }}
+                      />
+                    </Paper>
+                  </Grid>
+                ))
+              )}
             </Grid>
             <Grid item xs={12} md={7}>
               <Typography
@@ -285,8 +313,7 @@ export default function Home() {
                   textAlign: "center",
                 }}
               >
-                Colores realizados en ColorLucy, ven y solicita tu color con
-                nosotros
+                {textInfo.combinations_text}
               </Typography>
             </Grid>
           </Grid>
@@ -294,37 +321,41 @@ export default function Home() {
         {/* Apartado de productos */}
         <animated.div style={animatedPropsProductos}>
           <Grid
+            id="productos"
             container
             item
             xs={12}
             alignItems={"center"}
             justifyContent={"center"}
             marginTop="50px"
-            id="productos"
           >
             {/* Columna de la imagen */}
-            <Grid item xs={12} md={6} display="flex" justifyContent="center">
-              <Paper
-                elevation={0}
-                sx={{
-                  maxWidth: "530px",
-                  maxHeight: "600px",
-                  justifyContent: "center",
-                  display: "flex",
-                }}
-              >
-                <img
-                  src={"aerosoles.png"}
-                  alt={"aerosoles"}
-                  style={{
-                    width: "100%",
-                    maxHeight: "100%",
-                    objectFit: "cover",
-                    borderRadius: "10px",
+            {loadingProducts ? (
+              <></>
+            ) : (
+              <Grid item xs={12} md={6} display="flex" justifyContent="center">
+                <Paper
+                  elevation={0}
+                  sx={{
+                    maxWidth: "530px",
+                    maxHeight: "600px",
+                    justifyContent: "center",
+                    display: "flex",
                   }}
-                />
-              </Paper>
-            </Grid>
+                >
+                  <img
+                    src={productsImages[0].url}
+                    alt={"aerosoles"}
+                    style={{
+                      width: "100%",
+                      maxHeight: "100%",
+                      objectFit: "cover",
+                      borderRadius: "10px",
+                    }}
+                  />
+                </Paper>
+              </Grid>
+            )}
             {/* Columna del texto y botón */}
             <Grid
               item
@@ -343,7 +374,7 @@ export default function Home() {
                     fontStyle: "italic",
                   }}
                 >
-                  Encuentra los mejores productos para tu negocio.
+                  {textInfo.products_text}
                 </Typography>
               </div>
               <Button
@@ -380,17 +411,27 @@ export default function Home() {
                   textAlign: "center",
                 }}
               >
-                Contamos con los mejores aliados
+                {textInfo.allies_text}
               </Typography>
             </Grid>
             {/* logos marcas aliadas */}
             <Grid item xs={12} md={5} display={"flex"}>
-              <Card sx={{ maxWidth: 300, maxHeight: 230, marginRight: "15px" }}>
-                <CardMedia component="img" image="ppg.png" />
-              </Card>
-              <Card sx={{ maxWidth: 300, maxHeight: 230, marginRight: "15px" }}>
-                <CardMedia component="img" image="ixell.png" />
-              </Card>
+              {alliesImages == null ? (
+                <h3>{":("}</h3>
+              ) : (
+                alliesImages.map((image, index) => (
+                  <StyledCard
+                    key={index}
+                    sx={{
+                      maxWidth: 300,
+                      maxHeight: 230,
+                      marginRight: "16px",
+                    }}
+                  >
+                    <CardMedia component="img" image={image.url} />
+                  </StyledCard>
+                ))
+              )}
             </Grid>
           </Grid>
         </animated.div>
