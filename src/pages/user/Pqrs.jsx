c7889fbb48dab89c6c69df406a99a7b4d7497adf
Deisy Melo <deisymelo0127@gmail.com>
2024-06-08 19:04:00 -0500

    formulario p.q.r.s

---
+++ src/pages/user/Pqrs.jsx
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Grid,
+  TextField,
+  Button,
+  Typography,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+
+function Pqrs() {
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [formData, setFormData] = useState({
+    nombre: "",
+    apellido: "",
+    correo: "",
+    telefono: "",
+    asunto: "",
+    mensaje: "",
+  });
+
+  const [alert, setAlert] = useState({
+    open: false,
+    severity: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //proceso para enviar form
+  };
+
+  const handleCloseAlert = () => {
+    setAlert({ ...alert, open: false });
+  };
+
+  useEffect(() => {
+    const { nombre, apellido, correo, telefono, asunto, mensaje } = formData;
+    const isValid =
+      nombre.trim() !== "" &&
+      apellido.trim() !== "" &&
+      correo.trim() !== "" &&
+      telefono.trim() !== "" &&
+      asunto.trim() !== "" &&
+      mensaje.trim() !== "";
+    setIsFormValid(isValid);
+  }, [formData]);
+
+  return (
+    <Grid container spacing={1} alignContent={"flex-start"} padding={10}>
+      <Grid item xs={5} alignContent={"flex-start"}>
+        <Typography variant="h4">
+          <strong>PERMITENOS AYUDARTE</strong>
+        </Typography>
+        <Typography variant="body1">
+          <br />
+          En ColorLucy, tu satisfacción es nuestra prioridad.Si tienes alguna
+          petición, queja, reclamo o sugerencia, no dudes en comunicarte con
+          nosotros. Queremos escuchar tus comentarios y asegurarnos de brindarte
+          la mejor experiencia posible. <br />
+          <br />
+          Por favor, completa el siguiente formulario con tus datos y detalles
+          de tu solicitud. Nos comprometemos a responderte en el menor tiempo
+          posible.
+        </Typography>
+        <Typography variant="body2">
+          <em>
+            <br />
+            *Recuerda que todos los campos son obligatorios.
+          </em>
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Nombre completo"
+            name="nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Correo Electrónico"
+            name="correo"
+            type="email"
+            value={formData.correo}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Número de teléfono"
+            name="telefono"
+            type="tel"
+            value={formData.telefono}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+
+          <TextField
+            label="Asunto"
+            name="asunto"
+            value={formData.asunto}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Mensaje"
+            name="mensaje"
+            value={formData.mensaje}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            multiline
+            rows={4}
+            required
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={!isFormValid}
+          >
+            Enviar
+          </Button>
+          <input type="hidden" name="_catcha" value={false} />
+        </form>
+      </Grid>
+      <Snackbar
+        open={alert.open}
+        autoHideDuration={6000}
+        onClose={handleCloseAlert}
+      >
+        <Alert onClose={handleCloseAlert} severity={alert.severity}>
+          {alert.message}
+        </Alert>
+      </Snackbar>
+    </Grid>
+  );
+}
+
+export default Pqrs;
