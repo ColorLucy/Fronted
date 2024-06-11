78962d969927515165d35a20bda9b611de6b9292
Juan2040751 <viafara.juan@correounivalle.edu.co>
2024-06-10 20:52:46 -0500

    mejoras sprint 5

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -64,7 +64,7 @@ export const AuthProvider = ({ children }) => {
     setAuthTokens(null);
     setUser(null);
     localStorage.removeItem("authTokens");
-    navigate(`/admin/login?back=${backURL}`);
+    navigate(back ? `/admin/login?back=${backURL}` : "/admin/login");
   };
   const triggerLoginRedirect = () => {
     setShowLoginRedirect(true);
